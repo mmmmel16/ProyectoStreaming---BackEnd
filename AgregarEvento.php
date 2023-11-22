b9637d52fad302cdcf0ae973d6f561c6d2853744
Leonel <104234656+LeonelZ99@users.noreply.github.com>
2023-11-22 16:53:10 -0300

    Agregar Evento funcionando bien.

--- AgregarEvento.php
+++ AgregarEvento.php
@@ -19,7 +19,7 @@ if ($conexion->connect_error) {
 $data = json_decode(file_get_contents("php://input"), true);
 
 // Verifica si los datos se recibieron correctamente
-if (empty($data['name']) || empty($data['description']) || empty($data['imageUrl'])) {
+if (empty($data['nombre_evento']) || empty($data['tipo_deporte']) || empty($data['lugar_evento']) || empty($data['fecha_evento']) || empty($data['img_evento']) || empty($data['horario_evento']) || empty($data['url_transmision'])) {
     echo json_encode(array('message' => 'Error: Todos los campos son obligatorios.'));
     exit;
 }
@@ -31,15 +31,15 @@ $lugar_evento = $conexion->real_escape_string($data['lugar_evento']);
 $fecha_evento = $conexion->real_escape_string($data['fecha_evento']);
 $img_evento = $conexion->real_escape_string($data['img_evento']);
 $horario_evento = $conexion->real_escape_string($data['horario_evento']);
-$url_transimision = $conexion->real_escape_string($data['url_transmision']);
-
+$url_transmision = $conexion->real_escape_string($data['url_transmision']);
 
 // Consulta para insertar un nuevo evento
-$sql = "INSERT INTO eventodeporte (nombre_evento, tipo_deporte,lugar_evento,fecha_evento, img_evento, horario_evento, url_transimision) VALUES ('$nombre_evento', '$tipo_deporte', '$lugar_evento','$fecha_evento','$img_evento','$horario_evento','$url_transmision')";
-// Ajusta la consulta según la estructura real de tu tabla y los campos necesarios.
+$sql = "INSERT INTO eventodeporte (nombre_evento, tipo_deporte, lugar_evento, fecha_evento, img_evento, horario_evento, url_transmision) VALUES ('$nombre_evento', '$tipo_deporte', '$lugar_evento', '$fecha_evento', '$img_evento', '$horario_evento', '$url_transmision')";
 
 if ($conexion->query($sql) === TRUE) {
-    echo json_encode(array('message' => 'Evento agregado exitosamente'));
+    // Obtén el ID del evento recién insertado
+    $id_evento = $conexion->insert_id;
+    echo json_encode(array('message' => 'Evento agregado exitosamente', 'id_evento' => $id_evento));
 } else {
     echo json_encode(array('message' => 'Error al agregar el evento: ' . $conexion->error));
 }
